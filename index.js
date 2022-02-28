fccbba7b894d96bd119dc73cb76bba41ecd7348f
Adam <adam.crespo101@outlook.com>
2022-02-28 09:45:48 -0500

    commit tree 6b920cc4b64762a98e7b1423ca2cc09e61155664

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+//  function findMatching(array, string){
+//     return array.filter(
+//         str => str.toLowerCase() === string.toLowerCase()
+//     );
+// }
+
+// function fuzzyMatch(array, fuzzyString){
+//     return array.filter(
+//         str => str.indexOf(fuzzyString) === 0
+//     )
+// }
+
+// function matchName(array, matchedName){
+//     return array.filter(
+//         (home) => home.name === matchedName
+//     )
+// }
+
+function findMatching (array, string) {
+    return array.filter(
+        str => str.toLowerCase() === string.toLowerCase()
+    )
+}
+
+function fuzzyMatch (array, string){
+    return array.filter(
+        str => str.charAt(0) === string.charAt(0)
+    )
+}
+
+function matchName (array, string) {
+    return array.filter(
+        str => str.name === string
+    )
+}
